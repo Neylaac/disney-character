a3a04711cf18e95e0097821de6c791dda4d169c7
Neylaac <neyla.achaoui@student.ehb.be>
2026-05-09 16:10:59 +0200

    Display Disney characters on page

--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import "./style.css";
 
 const API_URL = "https://api.disneyapi.dev/character?page=1&pageSize=50";
 
+const app = document.querySelector("#app");
+
 const getDisneyCharacters = async () => {
   const response = await fetch(API_URL);
   const characters = await response.json();
+  
 
-  console.log(characters.data);
+   const characterCards = characters.data.map((character) => {
+    return `
+      <article>
+        <h2>${character.name}</h2>
+        <img src="${character.imageUrl}" alt="${character.name}">
+      </article>
+    `;
+  });
+  
+    app.innerHTML = characterCards.join("");
+
+
 };
 
 getDisneyCharacters();
